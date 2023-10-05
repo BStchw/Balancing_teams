13417c9b7d026776ef7cf149be3b8cac2bec4ff7
BStchw <93220422+BStchw@users.noreply.github.com>
2023-10-06 01:45:30 +0200

    Final

--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -2,8 +2,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class TeamTest {
     @Test
@@ -21,12 +20,8 @@ public class TeamTest {
 
         //then
         List<Individual> members = team.getMembers();
-        assertEquals(2, members.size(), "Team should have 2 members");
-        assertTrue(members.contains(individual1), "Team should contain individual1");
-        assertTrue(members.contains(individual2), "Team should contain individual2");
-
         int sumOfRates = team.getSumOfRates();
-        assertEquals(13, sumOfRates, "Sum of rates should be 13");
+        assertEquals(13, sumOfRates);
 
     }
 }
